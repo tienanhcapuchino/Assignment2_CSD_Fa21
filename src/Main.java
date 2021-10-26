d78d7f676111ccc8c149e90df671ec97abf02a0f
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-10-26 22:43:24 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
 
+
 import java.util.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ptuan
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+
+        String [] a = {"Hanoi", "longan", "tphcm"};
+
+        Scanner sc = new Scanner(System.in);
+        BusRouter b1 = new BusRouter("88", 1000000, a);
         int choice;
         ArrayList<BusRouter> bus = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
         ListChoice ls = new ListChoice();
         ProcessFile pro = new ProcessFile();
+        bus.add(b1);
         do{
             ls.menu();
             choice = sc.nextInt();
@@ -31,8 +24,13 @@ public class Main {
                         pro.loatFile(bus);
                         ls.disPlay(bus);
                     }
+                    break;
                 case 2 :
+                    ls.addRoute(bus);
+                    break;
                 case 3 :
+                    ls.disPlay(bus);
+                    break;
                 case 4 :
                 case 5 :  
             }
@@ -40,3 +38,4 @@ public class Main {
     }
     
 }
+

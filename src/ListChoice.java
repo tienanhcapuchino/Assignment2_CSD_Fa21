805a0cd1f8710642a00ac2d4fbf38501c4018a94
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-10-26 22:41:56 +0700

    update listchoice

--- src/ListChoice.java
+++ src/ListChoice.java
@@ -1,17 +1,12 @@
 
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ptuan
- */
 public class ListChoice {
+
+    Scanner sc = new Scanner(System.in);
+    ArrayList <BusRouter> lstBus = new ArrayList<>();
     ProcessFile pro = new ProcessFile();
     public void menu(){
         System.out.println("Bus Map?");
@@ -23,11 +18,40 @@ public class ListChoice {
         System.out.println("\t6.Exit");
         System.out.print(">Choice: ");
     }
-    public void addRoute(ArrayList<BusRouter> bus){
+    public void addSample(){
+        String []a= {"A", "G", "T"};
+        String []b= {"J", "G", "Y"};
+        String []c= {"K", "L", "N"};
+        BusRouter b0 = new BusRouter("xe1", 40, a);
+        BusRouter b1 = new BusRouter("xe2", 65, b);
+        BusRouter b2 = new BusRouter("xe3", 34, c);
+        lstBus.add(b0);
+        lstBus.add(b1);
+        lstBus.add(b2);
     }
+    
+    public void addRoute(ArrayList<BusRouter> bus){
+        String name;
+        int pricee;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number Bus: ");
+        name = sc.nextLine();
+        System.out.println("Enter Price : ");
+        pricee = Integer.parseInt(sc.nextLine()); 
+        System.out.println("So diem dung: ");
+        int n = Integer.parseInt(sc.nextLine());
+        String stop[] = new String[n];
+        for (int i = 0; i < n; i++) {
+             stop[i] = sc.nextLine();
+        }
+        bus.add(new BusRouter(name, pricee, stop));
+    }
+    
+    
     public void disPlay(ArrayList<BusRouter> bus){
         for (BusRouter bu : bus) {
             System.out.println(bu.getRouter()+"\t"+ bu.getPrice() +"\t"+ bu.Stop());
         }
     }
 }
+
